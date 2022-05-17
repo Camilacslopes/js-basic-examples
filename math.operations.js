01a7961da867a9021dd4d4b08a2d1f803aa8d4c1
Camila Lopes <camilacslopes@gmail.com>
2022-05-17 23:15:58 +0000

    feat: Basic example of a sum operation in JavaScript

---
+++ math.operations.js
@@ -0,0 +1,9 @@
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+
+function mySum (a, b) {
+   return a + b;
+}
+
+console.log(mySum(255,17)) //272
