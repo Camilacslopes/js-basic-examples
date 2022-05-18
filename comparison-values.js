fbf063704107f525cafbb3a65ce48e2b383cf507
Camila Lopes <camilacslopes@gmail.com>
2022-05-18 00:36:56 +0000

    feat: Create an example of function that compare values

---
+++ comparison-values.js
@@ -0,0 +1,8 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+function equalOrNot(a, b) {
+   return a === b;
+}
+
+console.log(equalOrNot(2, "2")) //false
+console.log(equalOrNot(2, 2)) //true
