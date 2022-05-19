84f25c75a4bd5718a85209597b500b3a5adc2b1c
Camila Lopes <camilacslopes@gmail.com>
2022-05-19 22:28:34 +0000

    feat: Create a function that return nth character of a string

---
+++ nth-character.js
@@ -0,0 +1,8 @@
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+function getCharacter(a, n){
+  return a[n - 1];
+}
+
+console.log(getCharacter("abcd",3)) //c
+console.log(getCharacter("JavaScript", 8)) //i
