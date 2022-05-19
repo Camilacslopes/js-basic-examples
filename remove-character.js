a72149d068ef5f3f33848ac745b9855951caf071
Camila Lopes <camilacslopes@gmail.com>
2022-05-19 23:24:23 +0000

    feat: Create an example of function that remove 3 last characters

---
+++ remove-character.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string (a) as argument
+// Remove the first 3 characters of a
+// Return the result
+function removeLetters(a) {
+  return a.slice(3);
+}
+
+console.log(removeLetters("code")) //e
+console.log(removeLetters("1234")) //4
