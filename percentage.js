1032db62706712d2286ea9e8027fc4b3ca6c2766
Camila Lopes <camilacslopes@gmail.com>
2022-05-25 00:32:28 +0000

    feature: Create a function to calculate the percentage of one number over anoother

---
+++ percentage.js
@@ -0,0 +1,9 @@
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
+
+function myPercentage(a, b) {
+    return b / 100 * a
+}
+
+console.log(myPercentage(9000,7)) //630
+console.log(myPercentage(20000,7)) //1400
