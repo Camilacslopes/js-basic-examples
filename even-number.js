69a8a164f1d664c8d8f018ee4097fcc7fdfb232a
Camila Lopes <camilacslopes@gmail.com>
2022-06-13 23:46:55 +0000

    feat: Create a function to check if a number is even or not

---
+++ even-number.js
@@ -0,0 +1,11 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+
+function isEven(a) {
+    return a % 2 === 0
+}
+
+console.log(isEven(24)) //true
+console.log(isEven(797)) //false
+
